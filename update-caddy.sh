e1e6df7948d0d1ee78edc6418e3ad9d2624bb487
Nia292 <73426965+Nia292@users.noreply.github.com>
2025-11-29 14:09:29 +0100

    chore: Updates repository for 1.2.1 (#3)
    * chore: Updates repository for 1.2.1
    
    * chore: updates permissions
    
    * chore: fixes check for version

---
+++ update-caddy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# check if there are modifications
+git diff-index --quiet HEAD
+if [ $? -gt 0 ]; then
+    echo "There are modifications to laci relevant files. Automatic update not possible:"
+    git diff-index --name-only HEAD
+    exit 1
+fi
+
+git pull
+./migrate.sh
+cp docker-compose.caddy.yaml docker-compose.yaml
+echo "docker-compose.yaml updated, pulling new images"
+docker compose --env-file .env pull
+echo "restarting deployment"
+docker compose --env-file .env down
+docker compose --env-file .env up -d
+echo "Update successful!"
