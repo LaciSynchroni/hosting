407fd07068d9ac37c0ee76f229b51781b1bc4df0
Nia292 <73426965+Nia292@users.noreply.github.com>
2026-05-02 20:11:54 +0200

    Update exec-into-postgres.sh

--- scripts/exec-into-postgres.sh
+++ scripts/exec-into-postgres.sh
@@ -2,8 +2,8 @@
 
 if command -v docker; then
   CONTAINER_ID=$(docker ps --format "{{.ID}}" --filter name="postgres")
-  docker exec -it "$CONTAINER_ID" /bin/bash -c "psql --user mare"
+  docker exec -it "$CONTAINER_ID" /bin/bash -c "psql --user laci"
 elif command -v podman; then
   CONTAINER_ID=$(podman ps --format "{{.ID}}" --filter name="postgres")
-  podman exec -it "$CONTAINER_ID" /bin/bash -c "psql --user mare"
+  podman exec -it "$CONTAINER_ID" /bin/bash -c "psql --user laci"
 fi
